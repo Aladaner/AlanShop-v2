d4fee12b6b91a110bf87c8fcf117b5dc3b9ca99a
Aladaner <solovej555@gmail.com>
2022-07-05 08:52:58 +0300

    slider

---
+++ src/js/slider-settings.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick( {
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        autoplay: true
+    });
+})

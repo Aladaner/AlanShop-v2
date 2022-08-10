13870aae1db7a3032a8387de6ca673ad8b0c8129
Aladaner <solovej555@gmail.com>
2022-08-10 15:46:52 +0300

    news block update

--- src/js/slider-settings.js
+++ src/js/slider-settings.js
@@ -1,9 +1,49 @@
 $(document).ready(function () {
-    $('.slider').slick( {
+    $('.main-banner').slick({
         arrows: true,
         dots: true,
         adaptiveHeight: true,
         slidesToShow: 1,
-        autoplay: true, 
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
+
+    $('.news-banner').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    })
 })

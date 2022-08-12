c70b88705ffb366f795103526e997e11c864de3c
Aladaner <solovej555@gmail.com>
2022-08-12 16:05:49 +0300

    goods update

--- src/js/slider-settings.js
+++ src/js/slider-settings.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
                 }
             },
             {
-                breakpoint: 425,
+                breakpoint: 500,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
@@ -45,5 +45,44 @@ $(document).ready(function () {
                 }
             }
         ]
-    })
+    });
+
+    $('.goods-slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
 })
